de78e24a94f29be55ea77af70ce0b08529816bc8
Matt Hicks <mhicks@redhat.com>
2012-10-25 18:57:47 -0400

    Adding script to call wait handler
    Calling in the template isn't working, so moving to a script.

---
+++ amazon/success_handler.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Important - the callback is only working
+# if the file data is sent up.  Probably related
+# to the newlines after the initial brace.
+cat <<EOF > /tmp/success_data
+{
+  "Status" : "SUCCESS",
+  "Reason" : "Configuration Complete",
+  "UniqueId" : "$1",
+  "Data" : "Component has completed configuration."
+}
+EOF
+
+echo "Calling wait URL..."
+curl -T /tmp/success_data $2
+echo "Done"
