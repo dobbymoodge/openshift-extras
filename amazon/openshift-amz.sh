6165ef5da0796dbc07d34a9ad4d14c21a494a6c4
Miciah Masters <miciah.masters@gmail.com>
2012-10-29 17:11:33 +0000

    Fix a sed syntax error in last commit

--- amazon/openshift-amz.sh
+++ amazon/openshift-amz.sh
@@ -910,8 +910,8 @@ configure_controller()
   # Configure the broker with the correct hostname, and point the broker
   # to the data store (the host running MongoDB).
   sed -i -e "s/^CLOUD_DOMAIN=.*$/CLOUD_DOMAIN=${domain}/;
-             s/^MONGO_HOST_PORT=.*$/MONGO_HOST_PORT=${datastore_hostname}:27017/;
-             s/^AUTH_SALT=.*/AUTH_SALT=${broker_auth_salt}" \
+             s/^MONGO_HOST_PORT=.*$/MONGO_HOST_PORT=\"${datastore_hostname}:27017\"/;
+             s/^AUTH_SALT=.*/AUTH_SALT=\"${broker_auth_salt}\"/" \
       /etc/openshift/broker.conf
 
   # If you change the MongoDB password of "mooo" to something else, be
