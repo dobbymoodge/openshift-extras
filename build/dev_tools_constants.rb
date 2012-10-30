cc3a57c95ae9a4f94ecad60450b06c8b0730547c
Dan McPherson <dmcphers@redhat.com>
2012-10-30 00:05:05 -0400

    more work getting devenv working

--- build/dev_tools_constants.rb
+++ build/dev_tools_constants.rb
@@ -24,11 +24,15 @@ SIBLING_REPOS = {'origin-server' => ['../origin-server'],
                  'rhc' => ['../rhc'],
                  'enterprise-install' => ["../#{File.basename(FileUtils.pwd)}"],
                  'origin-dev-tools' => ['../origin-dev-tools']}
-SIBLING_REPOS_GIT_URL = {'origin-server' => 'https://github.com/openshift/origin-server.git',
-                        'rhc' => 'https://github.com/openshift/rhc.git',
+SIBLING_REPOS_GIT_URL = {'origin-server' => 'git@github.com:openshift/origin-server.git',
+                        'rhc' => 'git@github.com:openshift/rhc.git',
                         'enterprise-install' => 'git@github.com:openshift/enterprise-install.git',
                         'origin-dev-tools' => 'git@github.com:openshift/origin-dev-tools.git'}
                         
 DEV_TOOLS_REPO = 'origin-dev-tools'
 DEV_TOOLS_EXT_REPO = 'enterprise-install'
 ADDTL_SIBLING_REPOS = SIBLING_REPOS_GIT_URL.keys - [DEV_TOOLS_REPO, DEV_TOOLS_EXT_REPO]
+
+BASE_RELEASE_BRANCH = 'libra-rhel-6.3'
+
+JENKINS_BUILD_TOKEN = 'libra1'
---
+++ misc/devenv/setup-devenv-repos.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+cat > /etc/yum.repos.d/epel.repo <<EOF
+[epel]
+name=Extra Packages for Enterprise Linux 6 - \$basearch
+baseurl=http://mirror1.ops.rhcloud.com/mirror/epel/6/\$basearch/
+        http://mirror2.ops.rhcloud.com/mirror/epel/6/\$basearch/
+failovermethod=priority
+enabled=1
+gpgcheck=0
+
+[epel-testing]
+name=Extra Packages for Enterprise Linux 6 - Testing - \$basearch
+baseurl=http://mirror1.ops.rhcloud.com/mirror/epel/testing/6/\$basearch/
+        http://mirror2.ops.rhcloud.com/mirror/epel/testing/6/\$basearch/
+failovermethod=priority
+enabled=0
+gpgcheck=0
+
+EOF
+
+cat > /etc/yum.repos.d/devenv.repo <<EOF
+[qpid]
+name=Qpid repo for Enterprise Linux 6 - $basearch
+baseurl=https://mirror1.ops.rhcloud.com/libra/qpid/\$basearch/Packages/
+        https://mirror2.ops.rhcloud.com/libra/qpid/\$basearch/Packages/
+failovermethod=priority
+enabled=1
+gpgcheck=1
+gpgkey=https://mirror1.ops.rhcloud.com/libra/li-signing.asc
+sslverify=0
+sslclientcert=/var/lib/yum/client-cert.pem
+sslclientkey=/var/lib/yum/client-key.pem
+
+[passenger]
+name=Passenger repo for Enterprise Linux 6
+baseurl=https://mirror1.ops.rhcloud.com/libra/passenger
+        https://mirror2.ops.rhcloud.com/libra/passenger
+failovermethod=priority
+enabled=1
+gpgcheck=0
+sslverify=0
+sslclientcert=/var/lib/yum/client-cert.pem
+sslclientkey=/var/lib/yum/client-key.pem
+
+[rhui-us-east-1-rhel-server-releases-i386]
+name=Red Hat Enterprise Linux Server 6 -i386 (RPMs)
+mirrorlist=https://rhui2-cds01.us-east-1.aws.ce.redhat.com/pulp/mirror/content/dist/rhel/rhui/server/6/\$releasever/i386/os
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-auxiliary
+sslverify=1
+sslclientkey=/etc/pki/entitlement/content-rhel6.key
+sslclientcert=/etc/pki/entitlement/product/content-rhel6.crt
+sslcacert=/etc/pki/entitlement/cdn.redhat.com-chain.crt
+includepkgs=java-1.6.0-openjdk* java-1.7.0-openjdk*
+
+[rhui-us-east-1-rhel-server-releases-optional-i386]
+name=Red Hat Enterprise Linux Server 6 Optional -i386 (RPMs)
+mirrorlist=https://rhui2-cds01.us-east-1.aws.ce.redhat.com/pulp/mirror/content/dist/rhel/rhui/server/6/\$releasever/i386/optional/os
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-release file:///etc/pki/rpm-gpg/RPM-GPG-KEY-redhat-auxiliary
+sslverify=1
+sslclientkey=/etc/pki/entitlement/content-rhel6.key
+sslclientcert=/etc/pki/entitlement/product/content-rhel6.crt
+sslcacert=/etc/pki/entitlement/cdn.redhat.com-chain.crt
+includepkgs=java-1.6.0-openjdk* java-1.7.0-openjdk*
+EOF
+
+cat >> /etc/yum.repos.d/devenv.repo <<EOF
+[devenv]
+name=Li repo for Enterprise Linux 6 - $basearch
+baseurl=https://mirror1.ops.rhcloud.com/libra/${2-libra-rhel-6.3}-${1-candidate}/\$basearch/
+        https://mirror2.ops.rhcloud.com/libra/${2-libra-rhel-6.3}-${1-candidate}/\$basearch/
+failovermethod=priority
+enabled=1
+gpgcheck=0
+gpgkey=https://mirror1.ops.rhcloud.com/libra/RPM-GPG-KEY-redhat-beta
+ggpkey=https://mirror1.ops.rhcloud.com/libra/RPM-GPG-KEY-redhat-release
+sslverify=0
+sslclientcert=/var/lib/yum/client-cert.pem
+sslclientkey=/var/lib/yum/client-key.pem
+
+[devenv-source]
+
+name=Li repo for Enterprise Linux 6 - $basearch
+baseurl=https://mirror1.ops.rhcloud.com/libra/${2-libra-rhel-6.3}-${1-candidate}/source/SRPMS/
+        https://mirror2.ops.rhcloud.com/libra/${2-libra-rhel-6.3}-${1-candidate}/source/SRPMS/
+failovermethod=priority
+enabled=0
+gpgkey=https://mirror1.ops.rhcloud.com/libra/RPM-GPG-KEY-redhat-beta
+ggpkey=https://mirror1.ops.rhcloud.com/libra/RPM-GPG-KEY-redhat-release
+gpgcheck=0
+sslverify=0
+sslclientcert=/var/lib/yum/client-cert.pem
+sslclientkey=/var/lib/yum/client-key.pem
+EOF
